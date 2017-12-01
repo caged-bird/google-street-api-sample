4a9e87d3a3fb81f5608db5e9706233c0ca91a027
kai0masanari <k-toyama@sumilab.org>
2017-12-01 14:01:09 +0900

    Update checkNameHouse

--- checkNameHouse.py
+++ checkNameHouse.py
@@ -4,34 +4,66 @@
 import numpy as np
 import cv2
 import sys
+import glob
+import csv
+import requests
+import json
+from const import get_slack_key
 
+img_paths = glob.glob("search_image/*")
+
+THRESHOLD = 0.8
 img_path = "ref_image/namehouse.jpg"
 img_path2 = "sample_image/namesample.jpg"
 
-img = cv2.imread(img_path)
-img2 = cv2.imread(img_path2)
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+score_lists = []
+max_score = 0
+max_list = []
 
-# 特徴量記述
-detector = cv2.AKAZE_create()
-kp1, des1 = detector.detectAndCompute(gray, None)
-kp2, des2 = detector.detectAndCompute(gray2, None)
+for img_path2 in img_paths:
+  img1 = cv2.imread(img_path)
+  img2 = cv2.imread(img_path2)
 
-# 比較器作成
-bf = cv2.BFMatcher(cv2.NORM_HAMMING)
+  # 特徴量記述
+  detector = cv2.AKAZE_create()
+  #detector = cv2.xfeatures2d.SIFT_create()
+  kp1, des1 = detector.detectAndCompute(img1, None)
+  kp2, des2 = detector.detectAndCompute(img2, None)
+  
+  # 比較器作成
+  bf = cv2.BFMatcher(cv2.NORM_HAMMING)
+  #bf = cv2.BFMatcher()
+  
+  # 画像への特徴点の書き込み
+  #matches = bf.match(des1, des2)
+  matches = bf.knnMatch(des1, des2, k=2)
+  
+  good_score = []
+  for m,n in matches:
+    if m.distance < THRESHOLD * n.distance:
+      good_score.append([m])
+  
+  filename = img_path2.replace("search_images/", "")  
+  c_score = len(good_score)
 
-# 画像への特徴点の書き込み
-matches = bf.match(des1, des2)
-matches = sorted(matches, key = lambda x:x.distance)
+  score_list = []
+  score_list.append(filename)
+  score_list.append(c_score)
+  score_lists.append(score_list)
 
-# 出力画像作成 表示
-h1, w1, c1 = img.shape[:3]
-h2, w2, c2 = img2.shape[:3]
-height = max([h1,h2])
-width = w1 + w2
-out = np.zeros((height, width, 3), np.uint8)
+  if c_score > max_score:
+    max_score = c_score
+    max_list = [filename, max_score]
 
-cv2.drawMatches(img, kp1, img2, kp2, matches[:50],out, flags=0)
-cv2.imshow("name", out)
-cv2.waitKey(0)
+result_csv = open("result.csv", "a")
+writer = csv.writer(result_csv, lineterminator="\n")
+writer.writerows(score_lists)
+
+url = "https://hooks.slack.com/services/" + get_slack_key()
+requests.post(url, data=json.dumps({
+    'username': "endnotifier",
+    'link_names': 1,
+    'attachments': [{
+        "text": u"最大値は"+max_list[0]+"の"+str(max_list[1])+"デス^^)",
+    }]
+}))
