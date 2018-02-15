cc2bf77ebf7a9b26b2e43cd14a6d460cfb19b8fd
kai0masanari <k-toyama@sumilab.org>
2018-02-15 16:03:03 +0900

    Update geImages.py

--- getImages.py
+++ getImages.py
@@ -3,6 +3,7 @@ import time
 import shutil
 import requests
 import json
+import argparse
 from constants.api_keys import KEY_SLACK
 from constants.api_keys import KEY_STREET
 from constants.geo_info import LAT_UNIT
@@ -16,6 +17,15 @@ headings = [0.0, 45.0, 90.0, 135.0, 180.0, 225.0, 270.0, 315.0]
 
 lati = TARGET.bottomleft[0]
 longi = TARGET.bottomleft[1]
+
+parser = argparse.ArgumentParser()
+parser.add_argument("out",
+                    action="store",
+                    type=str)
+
+args = parser.parse_args()
+dir_path = args.out
+
 while lati < TARGET.upright[0]:
     longi = TARGET.bottomleft[1]
     while longi < TARGET.upright[1]:
@@ -26,7 +36,7 @@ while lati < TARGET.upright[0]:
             if response.json()["status"] in "OK":
                 url = "https://maps.googleapis.com/maps/api/streetview?size="+str(SIZE)+"&location="+','.join([str(loc[0]), str(loc[1])])+"&heading="+str(heading)+"&pitch=" + str(PITCH) + "&fov=" + str(FOV) + "&zoom=1&key=" + KEY_STREET
                 response = requests.get(url, stream=True)
-                file_name = './images/' + ','.join([str(loc[0]), str(loc[1])]) + "," + str(heading) + '.jpg'
+                file_name = dir_path + ','.join([str(loc[0]), str(loc[1])]) + "," + str(heading) + '.jpg'
                 with open(file_name, 'wb') as out_file:
                     shutil.copyfileobj(response.raw, out_file)
                 print(file_name)
@@ -43,7 +53,6 @@ while lati < TARGET.upright[0]:
                 break
             else:
                 print("no iamge")
-            time.sleep(0.5)
         longi += LONG_UNIT
     lati += LAT_UNIT
 
@@ -55,3 +64,4 @@ requests.post(url, data=json.dumps({
         "text": u"画像収集終わりかい？",
     }]
 }))
+
